f3e28f54c7b3fb34f38d4b5a39dc66c9d2b84184
Eli Wrenn <ewrenn@pivotal.io>
2018-10-11 15:23:02 -0700

    Remove remaining API version check code
    Signed-off-by: Mike Kenyon <mkenyon@pivotal.io>

--- command/v3/ssh_command.go
+++ command/v3/ssh_command.go
@@ -1,12 +1,8 @@
 package v3
 
 import (
-	"net/http"
-
 	"code.cloudfoundry.org/cli/actor/sharedaction"
 	"code.cloudfoundry.org/cli/actor/v3action"
-	"code.cloudfoundry.org/cli/api/cloudcontroller/ccerror"
-	"code.cloudfoundry.org/cli/api/cloudcontroller/ccversion"
 	"code.cloudfoundry.org/cli/command"
 	"code.cloudfoundry.org/cli/command/flag"
 	"code.cloudfoundry.org/cli/command/translatableerror"
@@ -59,10 +55,6 @@ func (cmd *SSHCommand) Setup(config command.Config, ui command.UI) error {
 
 	ccClient, uaaClient, err := shared.NewClients(config, ui, true, "")
 	if err != nil {
-		if v3Err, ok := err.(ccerror.V3UnexpectedResponseError); ok && v3Err.ResponseCode == http.StatusNotFound {
-			return translatableerror.MinimumCFAPIVersionNotMetError{MinimumVersion: ccversion.MinVersionApplicationFlowV3}
-		}
-
 		return err
 	}
 
